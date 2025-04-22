d0a64659ab8598856f7036eab041bb1291318744
Mihai Hodis <mihaihodis98@gmail.com>
2025-04-22 15:37:24 +0300

    Adaugat pagina about

---
+++ about.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head><title>About</title></head>
+<body>
+    <h1>Despre aplicație</h1>
+    <p>Acest proiect a fost realizat pentru testarea funcționalităților CRUD.</p>
+</body>
+</html>
